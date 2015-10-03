9b0b1b285858548566f9e42c93738dd66d76057a
Mateus Revoredo <mateus.revoredo@gmail.com>
2015-10-03 00:49:21 -0300

    Finish facebook album import draft

--- _plugins/generator_archive.rb
+++ _plugins/generator_archive.rb
@@ -1,7 +1,10 @@
+require 'koala'
+require 'yaml'
+
 module Jekyll
 
   class ArchivePage < Page
-    def initialize(site, base, dir, speakers, sessions, title, schedule)
+    def initialize(site, base, dir, speakers, sessions, title, schedule, organizers)
       @site = site
       @base = base
       @dir = dir
@@ -12,25 +15,61 @@ module Jekyll
       self.data['speakers'] = speakers
       self.data['sessions'] = sessions
       self.data['schedule'] = schedule
+      self.data['organizers'] = organizers
 
       self.data['title'] = "Arquivo: #{title}"
       self.data['permalink'] = "/#{dir}/"
+      self.data['pictures'] = generate_facebook_albums(schedule[0])
     end
+
+    def generate_facebook_albums(schedule)
+      return if schedule['facebookAlbumId'].nil? || schedule['facebookAlbumId'].empty?
+      Koala.config.api_version = "v#{site.config['facebookApiVersion']}"
+      @oauth = Koala::Facebook::OAuth.new(site.config['facebookAppId'], site.config['facebookAppSecret'])
+      app_access_token = @oauth.get_app_access_token
+      @graph = Koala::Facebook::API.new(app_access_token)
+      pictures = []
+      @graph.get_connections(schedule['facebookAlbumId'], 'photos').each do |picture|
+        pictures.push(
+          'id' => @graph.get_picture(picture['id']),
+          'link' => @graph.get_connections(picture['id'], '', :fields => 'link')['link']
+        )
+      end
+
+      set_album_link(schedule['facebookAlbumId'])
+
+      return pictures
+    end
+
+    def set_album_link(album_id)
+      self.data['facebookAlbum'] = @graph.get_connections(album_id, '', :fields => 'link')['link']
+    end
+
   end
 
   class ArchivePageGenerator < Generator
     safe true
 
     def generate(site)
-        dir = 'previous'
-        site.data['previous'].each do |archive|
-          speakers = site.data['archive'][archive['folder']]['speakers']
-          sessions = site.data['archive'][archive['folder']]['sessions']
-          schedule = site.data['archive'][archive['folder']]['schedule']
+      if site.config['facebookAppId'].empty? ||
+        site.config['facebookAppId'].nil? ||
+        site.config['facebookApiVersion'].empty? ||
+        site.config['facebookApiVersion'].nil? ||
+        site.config['facebookAppSecret'].empty? ||
+        site.config['facebookAppSecret'].nil?
+        return
+      end
+      # ENV['JEKYLL_ENV'] != 'production'
+      # >
+      dir = 'previous'
+      site.data['previous'].each do |archive|
+        speakers = site.data['archive'][archive['folder']]['speakers']
+        sessions = site.data['archive'][archive['folder']]['sessions']
+        schedule = site.data['archive'][archive['folder']]['schedule']
+        organizers = site.data['archive'][archive['folder']]['organizers']
 
-          site.pages << ArchivePage.new(site, site.source, File.join(dir, archive['folder']), speakers, sessions, archive['title'], schedule)
-        end
+        site.pages << ArchivePage.new(site, site.source, File.join(dir, archive['folder']), speakers, sessions, archive['title'], schedule, organizers)
+      end
     end
   end
-
 end
