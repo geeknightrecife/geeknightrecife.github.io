360ad5793f6f4b7adf207444d0b6c731c64c8f23
Mateus Revoredo <mateus.revoredo@gmail.com>
2015-10-10 23:22:40 -0300

    Debug non generation of _site folder

--- _plugins/generator_archive.rb
+++ _plugins/generator_archive.rb
@@ -87,6 +87,8 @@ module Jekyll
 
       current_event_time = Time.parse(site.data['location']['eventEndTime'])
       current_formatted_event_time = current_event_time.strftime '%Y-%m-%d'
+      puts Time.now
+      puts current_event_time
       puts (Time.now > current_event_time) && !site.data['archive'].has_key?(current_formatted_event_time)
       if (Time.now > current_event_time) && !site.data['archive'].has_key?(current_formatted_event_time)
         created_folder = "_data/archive/#{current_formatted_event_time}/"
