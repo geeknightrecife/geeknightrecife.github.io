1f0a17c162217e1815916cac6ccb1136b443b981
Mateus Revoredo <mateus.revoredo@gmail.com>
2015-10-10 23:36:01 -0300

    Debug non generation of _site folder

--- _plugins/generator_archive.rb
+++ _plugins/generator_archive.rb
@@ -101,7 +101,7 @@ module Jekyll
         system "git tag #{current_formatted_event_time}"
         system 'git add .'
         system 'git commit -m "Snap CI #$SNAP_PIPELINE_COUNTER"'
-        system 'git push'
+        system 'git push && git push --tags'
 
       end
     end
