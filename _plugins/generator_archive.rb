cf33b4bf55306094569c2d7f4f9ae5d066952d72
Mateus Revoredo <mateus.revoredo@gmail.com>
2015-10-10 22:25:19 -0300

    Debug non generation of _site folder

--- _plugins/generator_archive.rb
+++ _plugins/generator_archive.rb
@@ -67,7 +67,7 @@ module Jekyll
     safe true
 
     def generate(site)
-      # archive_previous_event(site)
+      archive_previous_event(site)
       dir = 'previous'
       site.data['archive'].keys.each do |archive|
         speakers = site.data['archive'][archive]['speakers']
@@ -83,19 +83,22 @@ module Jekyll
     def archive_previous_event(site)
       current_event_time = Time.parse(site.data['location']['eventEndTime'])
       current_formatted_event_time = current_event_time.strftime '%Y-%m-%d'
-
-      if (Time.now > current_event_time) && !site.data['archive'].has_key?(current_formatted_event_time)
-        created_folder = "_data/archive/#{current_formatted_event_time}/"
-        Dir.mkdir created_folder
-        FileUtils.cp('_data/location.yml', "#{created_folder}location.yml")
-        FileUtils.cp('_data/organizers.yml', "#{created_folder}organizers.yml")
-        FileUtils.cp('_data/schedule.yml', "#{created_folder}schedule.yml")
-        FileUtils.cp('_data/sessions.yml', "#{created_folder}sessions.yml")
-        FileUtils.cp('_data/speakers.yml', "#{created_folder}speakers.yml")
-
-        exec( 'sed -i "s/preparingNextEvent: false/preparingNextEvent: true/g" _config.yml' )
-        exec( "git tag #{current_formatted_event_time}" )
-      end
+      puts current_formatted_event_time
+      puts current_event_time
+      puts Time.now
+      puts (Time.now > current_event_time) && !site.data['archive'].has_key?(current_formatted_event_time)
+      # if (Time.now > current_event_time) && !site.data['archive'].has_key?(current_formatted_event_time)
+      #   created_folder = "_data/archive/#{current_formatted_event_time}/"
+      #   Dir.mkdir created_folder
+      #   FileUtils.cp('_data/location.yml', "#{created_folder}location.yml")
+      #   FileUtils.cp('_data/organizers.yml', "#{created_folder}organizers.yml")
+      #   FileUtils.cp('_data/schedule.yml', "#{created_folder}schedule.yml")
+      #   FileUtils.cp('_data/sessions.yml', "#{created_folder}sessions.yml")
+      #   FileUtils.cp('_data/speakers.yml', "#{created_folder}speakers.yml")
+      #
+      #   exec( 'sed -i "s/preparingNextEvent: false/preparingNextEvent: true/g" _config.yml' )
+      #   exec( "git tag #{current_formatted_event_time}" )
+      # end
     end
   end
 end
